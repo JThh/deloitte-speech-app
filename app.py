ddcb1fccf3a9479d3c7e27fbe5e4d8909676e39e
JT Han <jiatong.han@u.nus.edu>
2021-06-27 12:41:21 +0800

    Modified forum-related files

--- app.py
+++ app.py
@@ -20,20 +20,6 @@ import dateparser
 
 st.set_page_config(layout="wide")
 
-CURRENT_YEAR = 2021
-TIME_RANGE = 5
-EXPLAINABLE_TXT = ['curve', 'peak']
-
-data = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-data.loc[:, 'Date'] = data.loc[:, 'Date'].apply(
-    lambda x: str(int(x[:4])+4)+x[4:])
-
-PEAK_TIME = data.loc[data['AAPL.High'] ==
-                     data['AAPL.High'].max(), 'Date'].values[0]
-PEAK_VALUE = data['AAPL.High'].max()
-
-CATEGORIES = ["category "+str(n) for n in range(1,9)]
 
 
 def process_text(txt):
@@ -252,6 +238,22 @@ def show_meaning(key):
 
 
 def main():
+
+    CURRENT_YEAR = 2021
+    TIME_RANGE = 5
+    EXPLAINABLE_TXT = ['curve', 'peak']
+
+    data = pd.read_csv(
+        'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+    data.loc[:, 'Date'] = data.loc[:, 'Date'].apply(
+        lambda x: str(int(x[:4])+4)+x[4:])
+
+    PEAK_TIME = data.loc[data['AAPL.High'] ==
+                        data['AAPL.High'].max(), 'Date'].values[0]
+    PEAK_VALUE = data['AAPL.High'].max()
+
+    CATEGORIES = ["category "+str(n) for n in range(1,9)]
+    
     st.title("Speech Powered BI Dashboard")
     st.write("")
     st.sidebar.header("BI Dashboard")
