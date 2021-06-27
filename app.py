d368b4a18eda745a14cc879023bc37e29ad6a5c6
JT Han <jiatong.han@u.nus.edu>
2021-06-27 12:33:42 +0800

    Modified forum-related files

--- app.py
+++ app.py
@@ -18,22 +18,6 @@ import dateparser
 
 # data = pd.read_csv(DATA_URL)
 
-st.set_page_config(layout="wide")
-
-CURRENT_YEAR = 2021
-TIME_RANGE = 5
-EXPLAINABLE_TXT = ['curve', 'peak']
-
-data = pd.read_csv(
-    'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
-data.loc[:, 'Date'] = data.loc[:, 'Date'].apply(
-    lambda x: str(int(x[:4])+4)+x[4:])
-
-PEAK_TIME = data.loc[data['AAPL.High'] ==
-                     data['AAPL.High'].max(), 'Date'].values[0]
-PEAK_VALUE = data['AAPL.High'].max()
-
-CATEGORIES = ["category "+str(n) for n in range(1,9)]
 
 
 def process_text(txt):
@@ -259,6 +243,24 @@ def main():
         st.markdown(
             "The data is **fake and only for demonstration purpose**. The data was latest updated in _February, 2021_.")
 
+
+    st.set_page_config(layout="wide")
+
+    CURRENT_YEAR = 2021
+    TIME_RANGE = 5
+    EXPLAINABLE_TXT = ['curve', 'peak']
+
+    data = pd.read_csv(
+        'https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+    data.loc[:, 'Date'] = data.loc[:, 'Date'].apply(
+        lambda x: str(int(x[:4])+4)+x[4:])
+
+    PEAK_TIME = data.loc[data['AAPL.High'] ==
+                        data['AAPL.High'].max(), 'Date'].values[0]
+    PEAK_VALUE = data['AAPL.High'].max()
+
+    CATEGORIES = ["category "+str(n) for n in range(1,9)]
+    
     result = ''    
     col1, col2 = st.beta_columns(2)
 
