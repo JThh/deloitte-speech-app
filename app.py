2787d736e6872d1613cebb1cc3ebee3e78e9c68c
JT Han <jiatong.han@u.nus.edu>
2021-07-09 14:29:19 +0800

    Commited

--- app.py
+++ app.py
@@ -340,6 +340,57 @@ def main():
         pass
 
     components.iframe("https://dash-gallery.plotly.host/dash-financial-report/news-and-reviews",height=500,scrolling=True)
+    components.html(
+        """
+    <script src="https://cdn.jsdelivr.net/npm/vega@5.20.2"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-lite@5.1.0"></script>
+    <script src="https://cdn.jsdelivr.net/npm/vega-embed@6.17.0"></script>
 
+    <style media="screen">
+      /* Add space between Vega-Embed links  */
+      .vega-actions a {
+        margin-right: 5px;
+      }
+    </style>
+
+    <h1>Template for Embedding Vega-Lite Visualization</h1>
+    <!-- Container for the visualization -->
+    <div id="vis"></div>
+
+    <script>
+      var vlSpec = {
+        $schema: 'https://vega.github.io/schema/vega-lite/v5.json',
+        data: {
+          values: [
+            {a: 'C', b: 2},
+            {a: 'C', b: 7},
+            {a: 'C', b: 4},
+            {a: 'D', b: 1},
+            {a: 'D', b: 2},
+            {a: 'D', b: 6},
+            {a: 'E', b: 8},
+            {a: 'E', b: 4},
+            {a: 'E', b: 7}
+          ]
+        },
+        mark: 'bar',
+        encoding: {
+          y: {field: 'a', type: 'nominal'},
+          x: {
+            aggregate: 'average',
+            field: 'b',
+            type: 'quantitative',
+            axis: {
+              title: 'Average of b'
+            }
+          }
+        }
+      };
+
+      vegaEmbed('#vis', vlSpec);
+    </script>
+        """,
+        height=600,
+    )
 
 main()
