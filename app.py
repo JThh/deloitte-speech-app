eac302f18cf5627d30f77fe6a355f5b3f08882c7
JT Han <jiatong.han@u.nus.edu>
2021-07-09 17:24:02 +0800

    Commited

--- app.py
+++ app.py
@@ -423,5 +423,23 @@ def main():
         st.table(chat1)
     except ValueError:
         st.title("Enter your name and message into the sidebar, and post!")
+    
+    
+    st.title('Counter Example')
+    if 'count' not in st.session_state:
+        st.session_state.count = 0
+        st.session_state.last_updated = datetime.time(0,0)
+
+    def update_counter():
+        st.session_state.count += st.session_state.increment_value
+        st.session_state.last_updated = st.session_state.update_time
+
+    with st.form(key='my_form'):
+        st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
+        st.number_input('Enter a value', value=0, step=1, key='increment_value')
+        submit = st.form_submit_button(label='Update', on_click=update_counter)
+
+    st.write('Current Count = ', st.session_state.count)
+    st.write('Last Updated = ', st.session_state.last_updated)
 
 main()
