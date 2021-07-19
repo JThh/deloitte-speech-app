a531acf047ee856e731d6e286cb3ab21667c71b5
JT Han <jiatong.han@u.nus.edu>
2021-07-19 13:59:04 +0800

    Fixed bugs

--- app.py
+++ app.py
@@ -355,8 +355,8 @@ def process_text_v2(txt):
             my_bar = st.progress(0)
 
             for percent_complete in range(100):
-                time.sleep(0.1)
-                my_bar.progress(percent_complete + 10)     
+                time.sleep(0.01)
+                my_bar.progress(percent_complete + 1)     
 
             image = Image.open('./assets/BDH_Finance.png')
             st.image(image)   
