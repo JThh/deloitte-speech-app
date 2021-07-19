2e32ff8c1e79a765c256a7ab814cbcf82da85cba
JT Han <jiatong.han@u.nus.edu>
2021-07-19 18:33:01 +0800

    Fixed bugs

--- app.py
+++ app.py
@@ -630,7 +630,11 @@ def main():
         col1,col2,col3 = st.beta_columns(3)
 
         with col1:
-            link = '[邮件](https://outlook.office.com/)'
+            link = '''
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+            <a href="#" class="fa fa-facebook"></a>
+            [邮件](https://outlook.office.com/)
+            '''
             st.markdown(link, unsafe_allow_html=True)
 
         with col2:
