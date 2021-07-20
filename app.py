2688f070e8d14191d75403322e64d451199e9c98
JT Han <jiatong.han@u.nus.edu>
2021-07-20 22:02:37 +0800

    Fixed bugs

--- app.py
+++ app.py
@@ -582,34 +582,43 @@ def process_text(txt):
 
         st.write('您可能还想看：')
 
-        col1, col2, col3, col4, col5 = st.beta_columns([3,5,2,4,5])
+        col1, col2 = st.beta_columns(2)
 
-        selection = ''
+        # selection = ''
         with col1:
             st.write()
-            if st.button('洗发水'):
+            with st.beta_expander('洗发水'):
                 addRecord('Alex', '洗发水')
-                selection = '洗发水'
-        
-        with col2:
-            st.write()
-            if st.button('空气清新剂'):
+                visualize('洗发水')
+            with st.beta_expander('空气清新剂'):
                 addRecord('Alex', '空气清新剂')
-                selection = '空气清新剂'
-
-        with col3:
-            st.write()
-            if st.button('纸巾'):
+                visualize('空气清新剂')
+            with st.beta_expander('纸巾'):
                 addRecord('Alex', '纸巾')
-                selection = '纸巾'  
-        
-        with col4:
-            st.write()
-            if st.button('所有产品'):
+                visualize('纸巾')
+            with st.beta_expander('所有产品'):
                 addRecord('Alex', '所有产品')
-                selection = '所有产品'  
+                visualize('所有产品')
+        
+        # with col2:
+        #     st.write()
+        #     if st.button('空气清新剂'):
+        #         addRecord('Alex', '空气清新剂')
+        #         selection = '空气清新剂'
 
-        visualize(selection)          
+        # with col3:
+        #     st.write()
+        #     if st.button('纸巾'):
+        #         addRecord('Alex', '纸巾')
+        #         selection = '纸巾'  
+        
+        # with col4:
+        #     st.write()
+        #     if st.button('所有产品'):
+        #         addRecord('Alex', '所有产品')
+        #         selection = '所有产品'  
+
+        # visualize(selection)          
 
 
 def main():
