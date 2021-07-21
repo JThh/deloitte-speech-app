c27a02d8ae030c4a59e8da2141ac4d714714e9ad
JT Han <jiatong.han@u.nus.edu>
2021-07-21 12:02:33 +0800

    Fixed bugs

--- app.py
+++ app.py
@@ -386,8 +386,8 @@ def show_profit():
             y=1.02,
             xanchor="right",
             x=1
-        ), height=400, weight=500)
-        st.plotly_chart(fig, use_container_width=True,height=400, weight=500)
+        ), height=400, width=500)
+        st.plotly_chart(fig, use_container_width=True,height=400, width=500)
     with col3:
         fig = px.line(df.iloc[-20:,:], x="date", y=df.columns[4],
                     hover_data={"date": "|%B %d, %Y"},
@@ -408,8 +408,8 @@ def show_profit():
             y=1.02,
             xanchor="right",
             x=1
-        ),height=400, weight=500)
-        st.plotly_chart(fig, use_container_width=True,height=400, weight=500)        
+        ),height=400, width=500)
+        st.plotly_chart(fig, use_container_width=True,height=400, width=500)        
 
 def show_meaning(query):
     if '最好' in query:
