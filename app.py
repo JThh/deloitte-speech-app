616c682bb17bf0ff48175434f8bd87be8b5ef6a3
JT Han <jiatong.han@u.nus.edu>
2021-07-21 12:03:12 +0800

    Fixed bugs

--- app.py
+++ app.py
@@ -335,37 +335,36 @@ def show_profit():
     col1, col2, col3 = st.beta_columns([1,4,4])
     df = px.data.stocks()
 
+    with col1:
+        fig = go.Figure()
 
-    # with col1:
-    #     fig = go.Figure()
+        fig.add_trace(go.Indicator(
+            value = 200,
+            delta = {'reference': 160},
+            gauge = {
+                'axis': {'visible': False}},
+            domain = {'row': 0, 'column': 0}))
 
-    #     fig.add_trace(go.Indicator(
-    #         value = 200,
-    #         delta = {'reference': 160},
-    #         gauge = {
-    #             'axis': {'visible': False}},
-    #         domain = {'row': 0, 'column': 0}))
+        fig.add_trace(go.Indicator(
+            value = 120,
+            gauge = {
+                'shape': "bullet",
+                'axis' : {'visible': False}},
+            domain = {'x': [0.05, 0.5], 'y': [0.15, 0.35]}))
 
-    #     fig.add_trace(go.Indicator(
-    #         value = 120,
-    #         gauge = {
-    #             'shape': "bullet",
-    #             'axis' : {'visible': False}},
-    #         domain = {'x': [0.05, 0.5], 'y': [0.15, 0.35]}))
-
-    #     fig.add_trace(go.Indicator(
-    #         mode = "number+delta",
-    #         value = 300,
-    #         domain = {'row': 0, 'column': 1}))
+        fig.add_trace(go.Indicator(
+            mode = "number+delta",
+            value = 300,
+            domain = {'row': 0, 'column': 1}))
 
 
-    #     fig.update_layout(
-    #         grid = {'rows': 3, 'columns': 1, 'pattern': "independent"},
-    #         template = {'data' : {'indicator': [{
-    #             'title': {'text': "Speed"},
-    #             'mode' : "number+delta+gauge",
-    #             'delta' : {'reference': 90}}]
-    #                             }})
+        fig.update_layout(
+            grid = {'rows': 3, 'columns': 1, 'pattern': "independent"},
+            template = {'data' : {'indicator': [{
+                'title': {'text': "Speed"},
+                'mode' : "number+delta+gauge",
+                'delta' : {'reference': 90}}]
+                                }}, width=100)
     with col2:
         fig = px.line(df.iloc[-20:,:], x="date", y=df.columns[3],
                     hover_data={"date": "|%B %d, %Y"},
